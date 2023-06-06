7d1ee744e2355a88949a165cf9a71d2ba0dd7e69
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-06-06 22:46:25 +0800

    created .NET API proj in 1st -c,  setup vs to C#

--- API/WeatherForecast.cs
+++ API/WeatherForecast.cs
@@ -1,3 +1,6 @@
+// this is where classes live inside the framework
+using API.Controllers;
+
 namespace API;
 
 public class WeatherForecast
@@ -9,4 +12,6 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
+
+    // var thing = new WeatherForecastController
 }
