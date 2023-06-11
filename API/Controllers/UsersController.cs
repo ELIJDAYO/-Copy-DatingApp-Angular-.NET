e927f4d91864396c19742ed361bf99986c1bc14a
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-06-11 23:42:24 +0800

    make api req async

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -15,15 +16,15 @@ namespace API.Controllers
         }
         [HttpGet]
         // return list of users
-        public ActionResult<IEnumerable<AppUser>> GetUsers(){
-            var users = _context.Users.ToList();
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            var users = await _context.Users.ToListAsync();
 
             return users;
         }
-
+ 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUsers(int id){
-            var user = _context.Users.Find(id);
+        public async  Task<ActionResult<AppUser>> GetUsers(int id){
+            var user = await _context.Users.FindAsync(id);
 
             return user;
         }
