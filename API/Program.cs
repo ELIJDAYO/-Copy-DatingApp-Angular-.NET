04aca9fe884ff17acdd15fd54674829acfeaf41d
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-08-14 18:36:41 +0800

    Fixed previous commit. Added CORS support for SSL

--- API/Program.cs
+++ API/Program.cs
@@ -10,8 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+// add services
+builder.Services.AddCors();
+// modify request to client
 
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 app.MapControllers(); //middleware, redirect which api endpoint t go
