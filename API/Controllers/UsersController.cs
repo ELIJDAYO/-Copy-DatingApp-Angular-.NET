32671bf4929c142be0355845e422296af700b227
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-08-21 17:49:44 +0800

    Added the authentication middleware

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,29 +1,36 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    // validated by jwt token
+    [Authorize]
     // send request from client to this controller
 
     public class UsersController : BaseAPIController
     {
         private readonly DataContext _context;
-        public UsersController(DataContext context){
+        public UsersController(DataContext context)
+        {
             _context = context;
-            
+
         }
+        [AllowAnonymous]
         [HttpGet]
         // return list of users
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
             var users = await _context.Users.ToListAsync();
 
             return users;
         }
- 
+
         [HttpGet("{id}")]
-        public async  Task<ActionResult<AppUser>> GetUsers(int id){
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
             var user = await _context.Users.FindAsync(id);
 
             return user;
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,17 +12,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // opt passed in fun
-builder.Services.AddDbContext<DataContext>(opt => {
+builder.Services.AddDbContext<DataContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // add services
 builder.Services.AddCors();
 // modify request to client
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        // how server checks the signin key 
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+        ValidateIssuer = false,
+        // issuer of the token is API server but it is not implemented to pass issuer's info
+        ValidateAudience = false
+    };
+});
 
 var app = builder.Build();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("https://localhost:4200"));
+
+// adding middleware
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 app.MapControllers(); //middleware, redirect which api endpoint t go
