94a636590ce30bfece97296aa881ac4667407cd4
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-09-02 01:23:31 +0800

    Added angular route guard but not working toastr

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import {CanActivateFn} from '@angular/router'
+import { Observable, map } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+
+
+export const authGuard: CanActivateFn = (router, state) => {
+  const accountService = inject(AccountService);
+  const toastr = inject(ToastrService)
+
+  return accountService.currentUser$.pipe(
+    map(user => {
+      if(user) return true;
+      toastr.error("You shall not pass")
+      return false
+    })
+  )
+}
