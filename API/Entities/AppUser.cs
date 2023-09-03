bad99bdca9591f976eca9d4efb963896177b3ff2
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-09-03 17:35:12 +0800

    Extended user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace API.Entities
 {
@@ -10,9 +7,20 @@ namespace API.Entities
     {
         public int Id { get; set; }
         [Required]
-        public string UserName {get; set;}
+        public string UserName { get; set; }
 
-        public byte[] PasswordHash{ get; set;}
-        public byte[] PasswordSalt{ get; set;}
-     }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos {get; set;} = new();
+    }
 }
