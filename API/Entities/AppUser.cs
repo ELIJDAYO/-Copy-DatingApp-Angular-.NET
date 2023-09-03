9560cdae41a6a563b8716d13752f09b7a2e7c226
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-09-03 17:41:28 +0800

    Added DateTime extension to calculate age

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using API.Extensions;
 
 
 namespace API.Entities
@@ -22,5 +23,9 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public List<Photo> Photos {get; set;} = new();
+
+        public int GetAge(){
+            return DateOfBirth.CalculateAge();   
+        }
     }
 }
