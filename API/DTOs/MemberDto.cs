c68663e6174fd56f32eb3d243b051185cde131d8
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-09-08 22:27:12 +0800

    Using automapper, still debugging

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -3,11 +3,7 @@ namespace API.DTOs
     public class MemberDto
     {
         public int Id { get; set; }
-        [Required]
         public string UserName { get; set; }
-
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
         public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
